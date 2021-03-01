58f7a9b08adb92be94cb46f72e475372c2137a7a
evosliza <l.voskanyan@intoo.am>
2021-03-01 13:52:22 +0400

    App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow, ShallowWrapper } from 'enzyme';
 import App from './App';
+import MainRoutes from './pages/MainRoutes';
+import { firebaseService } from './firebase';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(() => mockDispatch),
+}));
+
+const mockHandleAuthStateChange = jest.fn();
+firebaseService.handleAuthStateChange = mockHandleAuthStateChange;
+
+describe('<App />', () => {
+  let mockUseDispatch: jest.MockedFunction<any>;
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    mockUseDispatch = jest.requireMock('react-redux').useDispatch;
+    wrapper = shallow(<App />);
+  })
+
+  test('renders MainRoutes', () => {
+    expect(wrapper.find(MainRoutes)).toBeTruthy();
+  });
+
+  test('handles mockHandleAuthStateChange', () => {
+    expect(mockUseDispatch).toBeCalled();
+    expect(mockHandleAuthStateChange).toBeCalled();
+
+    // todo: check if the handler of authStateChange is dispatching the right thing
+  });
+})
